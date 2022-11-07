d0832de1a80639478c2431249aab614cdc548d32
Usuario <Usuario@MSI>
2022-11-07 18:54:52 +0100

    Added package Player, dependencies commented due to failure in the
    project(previous to our modification)

---
+++ src/main/java/org/springframework/samples/idus_martii/player/Player.java
@@ -0,0 +1,54 @@
+package org.springframework.samples.idus_martii.player;
+
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.springframework.samples.idus_martii.faccion.Faccion;
+import org.springframework.samples.idus_martii.mensaje.Mensaje;
+import org.springframework.samples.idus_martii.model.BaseEntity;
+import org.springframework.samples.idus_martii.statistics.Achievement;
+import org.springframework.samples.idus_martii.turno.Turno;
+import org.springframework.samples.idus_martii.user.User;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name ="player")
+public class Player extends BaseEntity{
+	
+	@OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "username", referencedColumnName = "username")
+	private User user;
+	
+	
+	/*
+	@ManyToMany
+	@JoinColumn(name = "achievement", referencedColumnName = "achievement")
+	Achievement achievement;*/
+	
+	/*
+	@ManyToMany(mappedBy = "faccion")
+	@JoinColumn(name = "faccion", referencedColumnName = "achievement")
+	Faccion faccion;*/
+	
+	/*
+	@ManyToMany(mappedBy = "turno")
+	@JoinColumn(name = "turno", referencedColumnName = "turno")
+	Turno turno;*/
+	
+	
+	/*
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "player")
+	private Set<Mensaje> mensajesS;*/
+	
+}
