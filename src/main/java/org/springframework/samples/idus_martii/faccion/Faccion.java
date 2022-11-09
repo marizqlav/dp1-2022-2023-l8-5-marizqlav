fb64ebae215605ce65fd06a9ec877402a2e475f8
pparr <pparr@10.100.136.38>
2022-11-09 13:36:15 +0100

    Cambios entidad faccion

--- src/main/java/org/springframework/samples/idus_martii/faccion/Faccion.java
+++ src/main/java/org/springframework/samples/idus_martii/faccion/Faccion.java
@@ -1,12 +1,13 @@
 package org.springframework.samples.idus_martii.faccion;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import org.springframework.samples.idus_martii.model.BaseEntity;
@@ -18,17 +19,18 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-
+@Table(name = "faccion")
 public class Faccion extends BaseEntity{
 	
-	private FaccionesEnumerado faccionesposibles;  //Facciones a elegir solo 2
+	private List<FaccionesEnumerado> faccionesposibles;  //Facciones a elegir solo 2
 	
 	@NotNull
 	private FaccionesEnumerado faccionselecionada;
-	
+
 	
 	//Esperando clase jugador
 	@ManyToOne(optional=false)
 	@JoinColumn(name = "player_id")
 	private Player player;
+	
 }
