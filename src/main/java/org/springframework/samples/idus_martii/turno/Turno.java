e68c38445147de58576140756029645accb69ffb
alemerpal <alemerpal@alum.us.es>
2022-11-10 16:19:13 +0100

    turno fixed

--- src/main/java/org/springframework/samples/idus_martii/turno/Turno.java
+++ src/main/java/org/springframework/samples/idus_martii/turno/Turno.java
@@ -3,11 +3,14 @@ package org.springframework.samples.idus_martii.turno;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import org.springframework.samples.idus_martii.jugador.Jugador;
 import org.springframework.samples.idus_martii.model.BaseEntity;
 import org.springframework.samples.idus_martii.ronda.Ronda;
 import lombok.Getter;
@@ -19,26 +22,33 @@ import lombok.Setter;
 @Table(name = "turno")
 public class Turno extends BaseEntity {
 	@NotNull
-	@NotEmpty
-    private String consul;
+	@OneToOne
+    private Jugador consul;
+
 	@NotNull
-	@NotEmpty
-    private String predor;
+	@OneToOne
+    private Jugador predor;
+
 	@NotNull
-	@NotEmpty
-    private String edil1;
+	@OneToOne
+	private Jugador edil1;
+
 	@NotNull
-	@NotEmpty
-    private String edil2;
+	@OneToOne
+    private Jugador edil2;
+
 	@Min(0)
 	@Max(2)
     private Integer votosTraidores;
+
 	@Min(0)
 	@Max(2)
     private Integer votosLeales;
+
 	@Min(0)
 	@Max(2)
     private Integer votosNeutrales;
+
 	@NotNull
 	@ManyToOne
 	@JoinColumn(name = "ronda_id")
