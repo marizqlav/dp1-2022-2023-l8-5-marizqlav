5b3f52e17c7fb835f8e8dd573e69f00e1f28fe86
Mario <marizqlav@alum.us.es>
2022-11-12 15:40:30 +0100

    Cambios en Repository, Controller y Service de Partida

--- src/main/java/org/springframework/samples/idus_martii/partida/PartidaRepository.java
+++ src/main/java/org/springframework/samples/idus_martii/partida/PartidaRepository.java
@@ -12,7 +12,7 @@ import org.springframework.samples.idus_martii.jugador.Jugador;
 
 @Repository
 public interface PartidaRepository extends CrudRepository<Partida, Integer> {
-
+	List<Partida> findAll();
     Optional<Partida> findById(Integer id);
 
     @Query("SELECT j FROM Partida p JOIN p.faccionesJugadoras f JOIN f.jugador j WHERE p.id LIKE :idPartida")
