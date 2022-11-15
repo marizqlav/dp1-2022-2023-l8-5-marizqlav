2ce85b736daff3db4bdc5bd306d176ec362ea89f
albdomrui <albdomrui@alum.us.es>
2022-11-15 11:28:36 +0100

    Añadido propiedad numRonda y que se cree una ronda a la vez que partida

--- src/main/java/org/springframework/samples/idus_martii/ronda/RondaRepository.java
+++ src/main/java/org/springframework/samples/idus_martii/ronda/RondaRepository.java
@@ -2,10 +2,19 @@ package org.springframework.samples.idus_martii.ronda;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface RondaRepository extends CrudRepository<Ronda, Integer>{
     List<Ronda> findAll();
+    
+    @Transactional  
+	@Modifying
+	@Query(value = "INSERT INTO ronda(num_ronda,partida_id) VALUES (:num_ronda,:idpartida)", nativeQuery = true)
+	Integer anadirRonda(@Param("num_ronda") int num_ronda, @Param("idpartida") int idpartida);
 }
