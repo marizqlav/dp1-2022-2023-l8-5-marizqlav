3ee6cce455e6de902ba6422595cd65fbf4da0de6
Mario <marizqlav@alum.us.es>
2022-12-10 01:16:08 +0100

    Adicion de save en PartidaService y los metodos show en TestController

--- src/test/java/org/springframework/samples/idus_martii/partida/PartidaControllerTest.java
+++ src/test/java/org/springframework/samples/idus_martii/partida/PartidaControllerTest.java
@@ -6,16 +6,32 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.samples.idus_martii.configuration.SecurityConfiguration;
+import org.springframework.samples.idus_martii.faccion.FaccionService;
+import org.springframework.samples.idus_martii.faccion.FaccionesEnumerado;
 import org.springframework.samples.idus_martii.jugador.JugadorService;
+import org.springframework.samples.idus_martii.mensaje.MensajeService;
+import org.springframework.samples.idus_martii.ronda.Ronda;
 import org.springframework.samples.idus_martii.ronda.RondaService;
 import org.springframework.samples.idus_martii.turno.TurnoService;
+import org.springframework.samples.idus_martii.turno.Estados.EspiarEstado;
+import org.springframework.samples.idus_martii.turno.Estados.EstablecerRolesEstado;
+import org.springframework.samples.idus_martii.turno.Estados.TerminarTurnoEstado;
+import org.springframework.samples.idus_martii.turno.Estados.VotarEstado;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 @WebMvcTest(controllers = PartidaController.class,
     excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
@@ -27,19 +43,83 @@ public class PartidaControllerTest {
     private MockMvc mockMvc;
 
     @MockBean
-    PartidaService partidaService;
-    @MockBean
-    JugadorService jugadorService;
-    @MockBean
-    RondaService rondaService;
-    @MockBean
-    TurnoService turnoService;
+    private PartidaService partidaService;
 
+    @MockBean
+    private JugadorService jugadorService;
+    
+    @MockBean
+    private RondaService rondaService;
+    
+    @MockBean
+    private TurnoService turnoService;
+    
+    @MockBean
+    private FaccionService faccionService;
+    
+    @MockBean
+    private MensajeService mensajeService;
+    
+    @MockBean
+    private EstablecerRolesEstado establecerRolesEstado;
+    
+    @MockBean
+    private VotarEstado votarEstado;
+    
+    @MockBean
+    private EspiarEstado espiarEstado;
+    
+    @MockBean
+    private TerminarTurnoEstado terminarTurnoEstado;
+
+	private static final int PARTIDA_ID = 1;
+
+
+	@BeforeEach
+	void setup() {
+		Lobby lobby = new Lobby();
+		Partida partida = new Partida();
+		partida.setLobby(lobby);
+		partida.setFechaCreacion(LocalDateTime.now());
+		partida.setFechaInicio(LocalDateTime.now().minusMinutes(5));
+		partidaService.save(partida);
+		LocalDateTime fechaCreacion = LocalDateTime.of(2022,9,18,10,30,2);
+		LocalDateTime fechaInicio = LocalDateTime.of(2022,9,18,10,35,2);
+		LocalDateTime fechaFin = LocalDateTime.of(2022,9,18,10,45,2);
+		List<Ronda> rondas = partida.getRondas();
+		Integer numeroJugadores = partida.getNumeroJugadores();
+		partida.setFaccionGanadora(FaccionesEnumerado.Leal);
+		partida.setFechaCreacion(fechaCreacion);
+		partida.setFechaInicio(fechaInicio);
+		partida.setFechaFin(fechaFin);
+		partida.setRondas(rondas);
+		partida.setNumeroJugadores(numeroJugadores);
+		given(partidaService.getPartidas()).willReturn(Lists.newArrayList(partida));
+	}
+
+	@WithMockUser
+    @Test
+    public void testShowPartidasFinalizadas() throws Exception {
+        mockMvc.perform(get("/partida/finalizadas")).
+            andExpect(status().isOk());
+    }
     @WithMockUser
     @Test
-    public void IniciarPartidaTest() throws Exception {
-        mockMvc.perform(get("/partida/juego/{partidaId}/iniciar", 1))
-            .andExpect(status().is3xxRedirection());
+    public void testShowDetallesPartida() throws Exception {
+        mockMvc.perform(get("/partida/"+PARTIDA_ID+"/detalles")).
+            andExpect(status().isOk());
+    }	
+    @WithMockUser
+    @Test
+    public void testShowPartidasDisponibles() throws Exception {
+        mockMvc.perform(get("/partida/disponibles")).
+            andExpect(status().isOk());
     }
+    @WithMockUser
+    @Test
+    public void testShowPartidasEnJuegos() throws Exception {
+        mockMvc.perform(get("/partida/enJuego")).
+            andExpect(status().isOk());
+    }	
 
 }
---
+++ src/test/java/org/springframework/samples/idus_martii/turno/TurnoControllerTest.java
@@ -0,0 +1,101 @@
+package org.springframework.samples.idus_martii.turno;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.idus_martii.configuration.SecurityConfiguration;
+import org.springframework.samples.idus_martii.faccion.FaccionesConverter;
+import org.springframework.samples.idus_martii.jugador.Jugador;
+import org.springframework.samples.idus_martii.partida.PartidaService;
+import org.springframework.samples.idus_martii.ronda.Ronda;
+import org.springframework.samples.idus_martii.turno.Estados.EspiarEstado;
+import org.springframework.samples.idus_martii.turno.Estados.EstablecerRolesEstado;
+import org.springframework.samples.idus_martii.turno.Estados.EstadoTurno;
+import org.springframework.samples.idus_martii.turno.Estados.TerminarTurnoEstado;
+import org.springframework.samples.idus_martii.turno.Estados.VotarEstado;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.assertj.core.util.Lists;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+@WebMvcTest(controllers = TurnoController.class,
+    excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
+                                            classes = WebSecurityConfigurer.class),
+    excludeAutoConfiguration = SecurityConfiguration.class)
+public class TurnoControllerTest {
+    	
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TurnoService turnoService;
+    
+    @MockBean
+    private PartidaService partidaService;
+    
+    @MockBean
+    private FaccionesConverter faccionesConverter;
+
+    @MockBean
+    private EstablecerRolesEstado establecerRolesEstado;
+    
+    @MockBean
+    private VotarEstado votarEstado;
+    
+    @MockBean
+    private EspiarEstado espiarEstado;
+    
+    @MockBean
+    private TerminarTurnoEstado terminarTurnoEstado;
+
+	@BeforeEach
+	void setup() {
+		Turno turno = new Turno();
+		Jugador j1=new Jugador();
+		Jugador j2=new Jugador();
+		Jugador j3=new Jugador();
+		Jugador j4=new Jugador();
+		Ronda ronda = new Ronda();
+		turno.setConsul(j1);
+		turno.setEdil1(j2);
+		turno.setEdil2(j3);
+		turno.setPredor(j4);
+		turno.setRonda(ronda);
+		turno.setVotosLeales(3);
+		turno.setVotosTraidores(3);
+		turno.setVotosNeutrales(3);
+		turnoService.save(turno);
+		given(turnoService.getTurnos()).willReturn(Lists.newArrayList(turno));
+	}
+
+	@WithMockUser
+	@Test
+	public void testShowTurnos() throws Exception {
+	    mockMvc.perform(get("/turnos/")).
+	   		andExpect(status().isOk());
+	}
+
+}
