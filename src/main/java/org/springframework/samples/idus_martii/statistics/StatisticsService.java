49cb0eb9ecf5d30453801f35ce645ccc8160e178
albdomrui <albdomrui@alum.us.es>
2022-12-15 16:18:34 +0100

    funcion duracion pService

--- src/main/java/org/springframework/samples/idus_martii/statistics/StatisticsService.java
+++ src/main/java/org/springframework/samples/idus_martii/statistics/StatisticsService.java
@@ -1,5 +1,7 @@
 package org.springframework.samples.idus_martii.statistics;
 
+import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -30,6 +32,39 @@ public class StatisticsService {
 	public int partidasTotales(Jugador jugador) {
 		return partidaRepo.findAllFinalizadasJugador(jugador.getId()).size();
 	}
+	
+	public Map<String, Duration> duracionPartidas(Jugador jugador){
+		 Map<String, Duration> stats = new HashMap<String, Duration>();
+    	 Duration min = null;
+    	 Duration max = null;
+    	 Duration sum = null;
+    	 for(Partida p : partidaRepo.findAllFinalizadasJugador(jugador.getId())) {
+    		 Duration duration = Duration.between(p.getFechaCreacion(), p.getFechaFin());
+    		 Duration durPartida = Duration.between(p.getFechaCreacion(), p.getFechaFin());
+    		 stats.put(p.getId().toString(), durPartida);
+    		 if(sum == null) {
+    			 min = duration;
+    	    	 max = duration;
+    	    	 sum = duration;
+    		 }
+    		 else {
+    			 if(duration.compareTo(min) <0) {
+    				 min = duration;
+    			 }
+    			 else if(duration.compareTo(max) >0) {
+    				 max = duration;
+    			 }
+    			 sum = sum.plus(duration);
+    		 }
+    	 }
+    	 stats.put("max", max);
+    	 stats.put("min", min);
+    	 stats.put("media", sum.dividedBy(partidaRepo.findAllFinalizadasJugador(jugador.getId()).size()));
+    	 return stats;
+		
+	}
+	
+	
 	
 
 }
