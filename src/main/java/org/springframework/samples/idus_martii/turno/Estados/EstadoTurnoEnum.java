9e22dce8748f2d4a765a26887a1dff7142a3b9a6
alemerpal <alemerpal@alum.us.es>
2023-01-10 17:50:32 +0100

    final partida

--- src/main/java/org/springframework/samples/idus_martii/turno/Estados/EstadoTurnoEnum.java
+++ src/main/java/org/springframework/samples/idus_martii/turno/Estados/EstadoTurnoEnum.java
@@ -10,5 +10,6 @@ public enum EstadoTurnoEnum {
     DescubiertoAmarillo,
     Recuento,
     ElegirFaccion,
-    TerminarTurno;
+    TerminarTurno,
+    FinalPartida;
 }
---
+++ src/main/java/org/springframework/samples/idus_martii/turno/Estados/FinalPartidaEstado.java
@@ -0,0 +1,51 @@
+package org.springframework.samples.idus_martii.turno.Estados;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.jpatterns.gof.StatePattern;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.samples.idus_martii.faccion.Faccion;
+import org.springframework.samples.idus_martii.faccion.FaccionesEnumerado;
+import org.springframework.samples.idus_martii.partida.Partida;
+import org.springframework.samples.idus_martii.partida.PartidaService;
+import org.springframework.samples.idus_martii.partida.GameScreens.FinalScreen;
+import org.springframework.samples.idus_martii.partida.GameScreens.GameScreen;
+import org.springframework.samples.idus_martii.turno.Turno;
+import org.springframework.stereotype.Component;
+
+@Component
+@StatePattern.ConcreteState
+public class FinalPartidaEstado implements EstadoTurno {
+
+    private FinalScreen screen;
+    private PartidaService partidaService;
+
+    @Autowired
+    FinalPartidaEstado(FinalScreen screen, @Lazy PartidaService partidaService) {
+        this.screen = screen;
+        this.partidaService = partidaService;
+    }
+
+    @Override
+    public void takeAction(Turno context) {
+        System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
+        Partida partida = context.getRonda().getPartida();
+
+        partida.setFechaFin(LocalDateTime.now());
+        partida.setFaccionGanadora(partidaService.getFaccionGanadora(partida));
+        partidaService.save(partida);
+    }
+
+    @Override
+    public EstadoTurnoEnum getNextState(Turno context) {
+        return null;
+    }
+
+    @Override
+    public GameScreen getGameScreen() {
+        return screen;
+    }
+    
+}
