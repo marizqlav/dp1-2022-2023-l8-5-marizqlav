24f00431d910f5a79f57d06c07bd0f8ea0b47766
pparr <pparr@Pablo.tendawifi.com>
2023-01-10 18:05:42 +0100

    ronda corregido

--- src/main/java/org/springframework/samples/idus_martii/ronda/RondaRepository.java
+++ src/main/java/org/springframework/samples/idus_martii/ronda/RondaRepository.java
@@ -2,13 +2,11 @@ package org.springframework.samples.idus_martii.ronda;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
+
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.samples.idus_martii.mensaje.Mensaje;
+
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+
 
 @Repository
 public interface RondaRepository extends CrudRepository<Ronda, Integer>{
